2a040ae6002ff3d05c66e1c277987c18bde245b5
Jhosban Barajas <jhosban8@gmail.com>
2025-06-01 13:41:39 -0500

    Rename Main class to InventoryManagementSystem and integrate Spring Boot application setup

---
+++ src/main/java/edu/unac/InventoryManagementSystem.java
@@ -0,0 +1,11 @@
+package edu.unac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSystem {
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryManagementSystem.class, args);
+    }
+}
